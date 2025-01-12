2e37cfebbcbe8a8ea36b089e38d988ee72932c36
telamonian <telamonian@users.noreply.github.com>
2025-01-12 07:22:27 -0500

    added HUD scene and script

---
+++ dodge_the_creeps/hud.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+# notifies main node that the button has been pressed
+signal start_game
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+func show_game_over():
+	show_message("Game Over")
+	# wait until the MessageTimer has counted down
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the creeps!"
+	$Message.show()
+	# make a one-shot timer and wait for it to finish
+	await get_tree().create_timer(1).timeout
+	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
